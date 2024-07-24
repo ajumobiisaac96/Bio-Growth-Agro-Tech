46773b7d03e75492adeddda6951681533113124a
ajumobiisaac96 <94201052+ajumobiisaac96@users.noreply.github.com>
2024-07-24 21:28:46 +0100

    responsiveness

--- js/signup.js
+++ js/signup.js
@@ -5,3 +5,20 @@ function login(){
 function signup(){
     location.href = '/pages/signup.html'
 }
+
+const toggleBtn = document.querySelector('.toggle_btn');
+const toggleBtnIcon = document.querySelector('.toggle_btn .icon');
+const dropDownMenu = document.querySelector('.dropdown_menu');
+
+
+dropDownMenu.style.display = 'none';
+
+toggleBtn.onclick = function() {
+    if (dropDownMenu.style.display === 'none') {
+        dropDownMenu.style.display = 'block';
+        toggleBtnIcon.className = 'fa-solid fa-xmark'; // Update icon to close
+    } else {
+        dropDownMenu.style.display = 'none';
+        toggleBtnIcon.className = 'fa-solid fa-bars'; 
+    }
+};
