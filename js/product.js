743d2b0bf816d1bba08cfa157d8ab08a07f6f2a4
ajumobiisaac96 <94201052+ajumobiisaac96@users.noreply.github.com>
2024-07-28 04:36:18 +0100

    rr

--- js/product.js
+++ js/product.js
@@ -5,3 +5,20 @@ function signup(){
 function login(){
     location.href = '../pages/login.html'
 }
+
+const toggleBtn = document.querySelector('.toggle_btn');
+const toggleBtnIcon = document.querySelector('.toggle_btn .icon');
+const dropDownMenu = document.querySelector('.dropdown_menu');
+
+
+dropDownMenu.style.display = 'none';
+
+toggleBtn.onclick = function() {
+    if (dropDownMenu.style.display === 'none') {
+        dropDownMenu.style.display = 'block';
+        toggleBtnIcon.className = 'fa-solid fa-xmark'; // Update icon to close
+    } else {
+        dropDownMenu.style.display = 'none';
+        toggleBtnIcon.className = 'fa-solid fa-bars'; 
+    }
+};
